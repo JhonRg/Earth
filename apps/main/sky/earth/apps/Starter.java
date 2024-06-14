d7b58ea050d565632dd86fdcb80cfbf7d9fc4d5a
JonRG <rgjonatan@gmail.com>
2024-06-14 21:43:42 +0200

    Fixing build test configuration Exposing the service for Tomcat and others.

--- apps/main/sky/earth/apps/Starter.java
+++ apps/main/sky/earth/apps/Starter.java
@@ -1,12 +1,33 @@
 package sky.earth.apps;
 
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import sky.earth.apps.manga.controller.MangaPutController;
+
 
 @SpringBootApplication
 public class Starter {
+    private static ApplicationContext applicationContext;
+
     public static void main(String[] args) {
         SpringApplication.run(Starter.class, args);
+        applicationContext = new AnnotationConfigApplicationContext(MangaPutController.class);
+
+        for (String name : applicationContext.getBeanDefinitionNames()) {
+            System.out.println(name);
+        }
+        AutowireCapableBeanFactory beanFactory = applicationContext.getAutowireCapableBeanFactory();
+        checkBeansPressence("manga");
     }
 
+    private static void checkBeansPressence(String... beans){
+        for (String bean : beans) {
+            System.out.println("Is" + bean + " in ApplicationContext: "
+                    + applicationContext.getBean(bean));
+        }
+    }
 }
