0407273ace4d36fbe0b042133549e5665b93e267
JonRG <rgjonatan@gmail.com>
2024-06-15 00:49:48 +0200

    Rest Controller for the user creation.

---
+++ apps/main/sky/earth/apps/mlv/user/controller/UserPutController.java
@@ -0,0 +1,29 @@
+package sky.earth.apps.mlv.user.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import sky.earth.mlv.usuarios.application.create.UserCreator;
+import sky.earth.mlv.usuarios.domain.User;
+
+@RestController("user")
+public class UserPutController {
+    UserCreator creator;
+
+    @PutMapping("/saveUser/{id}")
+    public ResponseEntity saveUser(@RequestParam String id, @RequestBody Request request){
+        creator.create(id, request.getUserName(), request.getPassword());
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+}
+@Getter
+@Setter
+final class Request{
+    private String userName;
+    private String password;
+}
